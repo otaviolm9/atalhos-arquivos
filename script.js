22cf171b1fe350d96282542834ceae6df40bd27b
Otávio Lemos Morais <otaviolmorais1711@gmail.com>
2025-04-10 20:30:07 -0300

    Versão: 1.0.4 ALFA
    10/04/2025

---
+++ script.js
@@ -0,0 +1,8 @@
+/* VERSÃO: 1.0.4 ALFA */
+
+window.alert("ESSE SITE FOI FEITO SOB LICENÇA: MIT License")
+window.alert("ACESSE O CODIGO FONTE EM: https://github.com/otaviolm9/atalhos-arquivos")
+window.alert("NÃO RESPONSABILIZAMOS POR ERRO NA MAQUINA POR EFEITOS DESTE SITE")
+window.alert("ESSE SITE NÃO ESTÁ EM FUNCIONAMENTO!!!")
+window.alert("ESSE SITE NÃO ESTÁ EM FUNCIONAMENTO!!!")
+window.alert("ESSE SITE NÃO ESTÁ EM FUNCIONAMENTO!!!")
